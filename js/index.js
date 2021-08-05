a31743e633e371e4c7f179e936cc0a07f059bf57
andricalquhoun <andricalquhoun@gmail.com>
2021-08-04 20:25:08 -0400

    Aug 4th 8:25pm

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
-$('.li').on("click", function(){
+$('li').on("click", function(){
   
     if ($(this).hasClass("home")){
       $(".home").addClass("active");
-      $(".portfolio").removeClass("active");
       $(".home").css("display", "block");
-      $(".portfolio").css("display", "none");
     }
-    else if ($(this).hasClass("tab2")){
-      $(".tab2").addClass("active");
-      $(".tab1").removeClass("active");
-      $("#tabContent2").css("display", "block");
-      $("#tabContent1").css("display", "none");
+    else if ($(this).hasClass("portfolio")){
+      $(".portfolio").addClass("active");
+      $(".portfolio").css("display", "block");
     }
   });
