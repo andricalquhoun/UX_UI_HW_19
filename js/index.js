067f0fc43c011a01c1d503c3fb810d9c0ac16a66
andricalquhoun <andricalquhoun@gmail.com>
2021-08-06 12:21:35 -0400

    Aug 6th 12:19pm

--- js/index.js
+++ js/index.js
@@ -2,20 +2,12 @@ $('li').on("click", function(){
   
     if ($(this).hasClass("home")){
       $(".home").css("text-decoration", "underline");
+    } else if ($(this).hasClass("portfolio")){
+      $(".home").css("text-decoration", "overline");
     }
     if ($(this).hasClass("portfolio")){
       $(".portfolio").css("text-decoration", "underline");
-    }
-    if ($(this).hasClass("skill")){
-      $(".skill").css("text-decoration", "underline");
-    }
-    if ($(this).hasClass("resume")){
-      $(".resume").css("text-decoration", "underline");
-    }
-    if ($(this).hasClass("experience")){
-      $(".experience").css("text-decoration", "underline");
-    }
-    if ($(this).hasClass("contact")){
-      $(".contact").css("text-decoration", "underline");
+    } else if ($(this).hasClass("home")){
+      $(".portfolio").css("text-decoration", "overline");
     }
   });
